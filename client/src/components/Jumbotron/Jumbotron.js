527bf7a9d97bfd4c7dfadabdf54eb697223928f6
Lili <iliyanastefanova@msn.com>
2019-05-14 18:17:16 -0400

    initial login page

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -6,7 +6,7 @@ const Jumbotron = () => (
         <div className="row">
             <div className="col s12 l4 push-l1">
                 <div class="about">
-                Meet people, who share your passion for different activities and make lifetime friends.
+                Meet people, who share your passion for different sports activities and make lifetime friends.
                 </div>
                 <br></br>
                 <button className="btn waves-effect waves-light" type="submit" name="action">register
