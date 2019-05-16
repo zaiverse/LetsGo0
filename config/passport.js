2bddf6b890041cc6c3f27407305f153dd6e95421
zaiverse <brenda.garcia007@mymdc.net>
2019-05-16 19:04:11 -0400

    adding routes/api, server.js, models, controller

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+module.exports = {
+    mongoURI: "mongodb://letsgoapp-shard-00-00-59ctt.mongodb.net:27017",
+    secretOrKey: "secret"
+  };
+
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  location: { type: String, required: true },
+  profilePic: String
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
