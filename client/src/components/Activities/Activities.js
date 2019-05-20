7eac1ed2e6a4914e841dd4e6c6d23a562a4301ba
carlos <cacev000@gmail.com>
2019-05-20 19:01:46 -0400

    added activities page right before registering user and saving data except for sports that is not been saved

--- client/src/components/Activities/Activities.js
+++ client/src/components/Activities/Activities.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
 import "./Activities.css";
 
 class Activities extends Component {
-    state = {
-
+    constructor() {
+        super();
+        this.state = {
+            sports: []
+        };
     }
+
+    componentDidMount() {
+        // If logged in and user navigates to activities page, should redirect them to dashboard
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.push("/profile");
+        }
+    }
+
+    selectedSport = e => {
+        const indexOfSport = this.state.sports.indexOf(e.currentTarget.value);
+        if (indexOfSport < 0) {
+            e.currentTarget.style.backgroundColor = '#2bbbad';
+            this.state.sports.push(e.currentTarget.value);
+        } else {
+            e.currentTarget.style.backgroundColor = '#e7e3dd';
+            this.state.sports.splice(indexOfSport, 1);
+        }
+    }
+
+    createUser = e => {
+        e.preventDefault();
+        let newUser = JSON.parse(localStorage.getItem("userData"));
+        newUser.sports = this.state.sports;
+
+        this.props.registerUser(newUser, this.props.history);
+    };
+
     render() {
         return (
             <div className="container activities center">
@@ -12,27 +46,42 @@ class Activities extends Component {
             <p>(select all that apply)</p>
             <br></br>
                 <div className="row col l12 center">
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>running</button>
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>basketball</button>
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>football</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="running"><i className="material-icons right"></i>running</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="basketball"><i className="material-icons right"></i>basketball</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="football"><i className="material-icons right"></i>football</button>
                 </div>
                 <div className="row col l12">
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>tennis</button>
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>beach yoga</button>
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>volleyball</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="tennis"><i className="material-icons right"></i>tennis</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="beach yoga"><i className="material-icons right"></i>beach yoga</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="volleyball"><i className="material-icons right"></i>volleyball</button>
                 </div>
                 <div className="row col l12">
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>golf</button>
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>soccer</button>
-                    <button className="sportButton btn-large"><i className="material-icons right"></i>baseball</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="golf"><i className="material-icons right"></i>golf</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="soccer"><i className="material-icons right"></i>soccer</button>
+                    <button className="sportButton btn-large" onClick={this.selectedSport} value="baseball"><i className="material-icons right"></i>baseball</button>
                 </div>
                 <br></br>
                 <a href="/profile">
-                <button className="btn waves-effect waves-light" type="submit" name="action">register
-                <i className="material-icons right">send</i>
+                <button className="btn waves-effect waves-light" onClick={this.createUser} name="action">register
+                    <i className="material-icons right">send</i>
                 </button></a>
             </div>            
         )
     }
 }
-export default Activities;
+
+Activities.propTypes = {
+    registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    // errors: state.errors
+});
+
+export default connect(
+    mapStateToProps, {
+        registerUser
+    }
+)(withRouter(Activities));
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,8 +1,17 @@
-import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, {
+  Component
+} from "react";
+import {
+  Link,
+  withRouter
+} from "react-router-dom";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { registerUser } from "../../actions/authActions";
+import {
+  connect
+} from "react-redux";
+import {
+  registerUser
+} from "../../actions/authActions";
 import classnames from "classnames";
 
 
@@ -35,12 +44,14 @@ class Register extends Component {
       });
     }
   }
-onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
+  onChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   };
-onSubmit = e => {
+  onSubmit = e => {
     e.preventDefault();
-const newUser = {
+    const newUser = {
       name: this.state.name,
       username: this.state.username,
       email: this.state.email,
@@ -49,53 +60,60 @@ const newUser = {
       location: this.state.location,
       profilePic: this.state.profilePic,
     };
-this.props.registerUser(newUser, this.props.history); 
+
+    localStorage.setItem("userData", JSON.stringify(newUser));
+    this.props.history.push("/activities");
   };
 
-    render() {
-        const { errors } = this.state;
-        return (
-            <div className="container register">
-                <form noValidate onSubmit={this.onSubmit} className="white">
-                    <h5>Register</h5>
-                    <br></br>
-                    <div class="input-field">
-                    <label for="name">first and last name</label>
-                    <input onChange={this.onChange} value={this.state.name} error={errors.name} placeholder="john smith" id="name" type="text" className={classnames("", {invalid: errors.name})}/>
-                    <span className="red-text">{errors.name}</span>
-                    </div>
-                    <div class="input-field">
-                    <label for="username">Username</label>
-                    <input onChange={this.onChange} value={this.state.username} error={errors.username} placeholder="timjohns23" id="username" type="text" className={classnames("", {invalid: errors.username})}/>
-                    <span className="red-text">{errors.username}</span>
-                    </div>
-                    <div className="input-field">
-                    <label htmlFor="email">email</label>
-                    <input onChange={this.onChange} value={this.state.email} error={errors.email} placeholder="john@john.com" id="email" type="email" className={classnames("", {invalid: errors.email})}/>
-                    <span className="red-text">{errors.email}</span>
-                    </div>
-                    <div className="input-field">
-                    <label htmlFor="password">password</label>
-                    <input onChange={this.onChange} value={this.state.password} error={errors.password} placeholder="type your password" id="password" type="password" className={classnames("", {invalid: errors.password})}/>
-                    <span className="red-text">{errors.password}</span>
-                    </div>
-                    <div className="input-field">
-                    <label htmlFor="password2">confirm password</label>
-                    <input onChange={this.onChange} value={this.state.password2} error={errors.password2} placeholder="retype password" type="password" id="password2" className={classnames("", {invalid: errors.password2})}/>
-                    <span className="red-text">{errors.password2}</span>
-                    </div>
-                    <div class="input-field">
-                    <input onChange={this.onChange} value={this.state.location} error={errors.location} placeholder="city, state or zip code" id="location" type="text" className={classnames("", {invalid: errors.location})}/>
-                    <label for="location">location</label>
-                    <span className="red-text">{errors.location}</span>
-                    </div>
-                    <div><i class="small material-icons" id="uploadPhoto">add_a_photo</i> add a photo</div>
-                    <div className="input-field">
-                    <button className="btn waves-effect waves-light" type="submit">next<i class="material-icons right">send</i></button></div>
-                </form>
-            </div>            
-        )
-    }
+  render() {
+    const {
+      errors
+    } = this.state;
+    return ( 
+        <div className="container register">
+          <form noValidate onSubmit={this.onSubmit} className="white">
+              <h5>Register</h5>
+              <br></br>
+              <div className="input-field">
+                <label htmlFor="name">first and last name</label>
+                <input onChange={this.onChange} value={this.state.name} error={errors.name} placeholder="john smith" id="name" type="text" className={classnames("", {invalid: errors.name})}/>
+                <span className="red-text">{errors.name}</span>
+              </div>
+              <div className="input-field">
+                <label htmlFor="username">Username</label>
+                <input onChange={this.onChange} value={this.state.username} error={errors.username} placeholder="timjohns23" id="username" type="text" className={classnames("", {invalid: errors.username})}/>
+                <span className="red-text">{errors.username}</span>
+              </div>
+              <div className="input-field">
+                <label htmlFor="email">email</label>
+                <input onChange={this.onChange} value={this.state.email} error={errors.email} placeholder="john@john.com" id="email" type="email" className={classnames("", {invalid: errors.email})}/>
+                <span className="red-text">{errors.email}</span>
+              </div>
+              <div className="input-field">
+                <label htmlFor="password">password</label>
+                <input onChange={this.onChange} value={this.state.password} error={errors.password} placeholder="type your password" id="password" type="password" className={classnames("", {invalid: errors.password})}/>
+                <span className="red-text">{errors.password}</span>
+              </div>
+              <div className="input-field">
+                <label htmlFor="password2">confirm password</label>
+                <input onChange={this.onChange} value={this.state.password2} error={errors.password2} placeholder="retype password" type="password" id="password2" className={classnames("", {invalid: errors.password2})}/>
+                <span className="red-text">{errors.password2}</span>
+              </div>
+              <div className="input-field">
+                <input onChange={this.onChange} value={this.state.location} error={errors.location} placeholder="city, state or zip code" id="location" type="text" className={classnames("", {invalid: errors.location})}/>
+                <label htmlFor="location">location</label>
+                <span className="red-text">{errors.location}</span>
+              </div>
+              <div>
+                <i className="small material-icons" id="uploadPhoto">add_a_photo</i> add a photo
+              </div>
+              <div className="input-field">
+                <button className="btn waves-effect waves-light" type="submit">next<i className="material-icons right">send</i></button>
+              </div>
+          </form>
+      </div>       
+    )
+  }
 }
 
 Register.propTypes = {
@@ -110,6 +128,7 @@ const mapStateToProps = state => ({
 });
 
 export default connect(
-  mapStateToProps,
-  { registerUser }
+  mapStateToProps, {
+    registerUser
+  }
 )(withRouter(Register));
