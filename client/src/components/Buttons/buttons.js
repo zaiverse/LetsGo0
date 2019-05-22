01e8d6e927e9c77e3bd100b8e56fb2fc4ae96251
Lili <iliyanastefanova@msn.com>
2019-05-21 22:09:11 -0400

    fixed animation

--- client/src/components/Buttons/buttons.js
+++ client/src/components/Buttons/buttons.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import logo from"../../images/logo_7.png";
+import logo from"../../images/logo_no_ball.png";
+import ball from"../../images/ball.png";
 import { logoutUser } from "../../actions/authActions";
 
 class Buttons extends Component{
@@ -14,7 +15,8 @@ class Buttons extends Component{
     render(){
         return(
             <div className="nav-wrapper">
-            <a href="/" className="responsive-img"><img src={logo} alt="Logo" /></a>
+            <a href="/" className="responsive-img"><img className="logo" src={logo} alt="Logo" /></a>
+            <a href="/" className="responsive-img"><img className="ball animated bounce" src={ball} alt="Logo" /></a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a  onClick={this.onLogoutClick}>log out</a></li>
                     <li><a href="/profile"><i className="material-icons">account_circle</i></a></li>
