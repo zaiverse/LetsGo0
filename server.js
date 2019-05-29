623fbcc75da9334d3a30bb45b8abe865a75381d3
Brenda Garcia <brenda.garcia007@mymdc.net>
2019-05-28 21:26:29 -0400

    making sch

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
@@ -38,5 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
