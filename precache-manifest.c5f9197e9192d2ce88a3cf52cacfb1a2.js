19d1f40c3cd2879dac71d0d451f07c2e0d4f32ae
Brenda Garcia <brenda.garcia007@mymdc.net>
2019-05-28 20:10:39 -0400

    Updates

---
+++ precache-manifest.c5f9197e9192d2ce88a3cf52cacfb1a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65367b79043a801ab97160d522079b46",
+    "url": "/LetsGo0/index.html"
+  },
+  {
+    "revision": "f5a46be242a1db590d41",
+    "url": "/LetsGo0/static/css/main.5ae16a63.chunk.css"
+  },
+  {
+    "revision": "71a0060709f828621a3e",
+    "url": "/LetsGo0/static/js/2.6f07b7b8.chunk.js"
+  },
+  {
+    "revision": "f5a46be242a1db590d41",
+    "url": "/LetsGo0/static/js/main.1770891f.chunk.js"
+  },
+  {
+    "revision": "0e100d0ea9925c02e299",
+    "url": "/LetsGo0/static/js/runtime~main.7f51b65e.js"
+  },
+  {
+    "revision": "96f73bd31d6c560158b9d1293fa814c3",
+    "url": "/LetsGo0/static/media/background.96f73bd3.svg"
+  },
+  {
+    "revision": "b581f233c6681d9d298b92878d50df04",
+    "url": "/LetsGo0/static/media/ball.b581f233.png"
+  },
+  {
+    "revision": "da9035ea5485b4bd03056e62cb046c27",
+    "url": "/LetsGo0/static/media/logo_no_ball.da9035ea.png"
+  },
+  {
+    "revision": "fdf3d85d0fb5a80b7a3b20aca5fc2442",
+    "url": "/LetsGo0/static/media/pexels-photo-364308.fdf3d85d.jpeg"
+  }
+]);
